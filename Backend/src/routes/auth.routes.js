dfbaacb3123f04c12d9c51deb5502cbb0e39af8c
ReyMartinez2002 <frbsfrbs1@gmail.com>
2025-07-18 17:14:13 -0500

    actualizo

--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/auth.controller');
 const authenticateToken = require('../middleware/auth.middleware');
-const allowRoles = require('../middleware/roles.middleware');
+const allowRoles = require('../middleware/roles.middleware'); 
 
-// Registro de usuario (solo superadmin)
 router.post('/register', authenticateToken, allowRoles('superadmin'), authController.register);
+router.post('/login', authController.login);
+router.get('/usuarios', authController.getUsuarios);
 
 module.exports = router;
